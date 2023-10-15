dc6c1bb6d8e1ffeb2a5ecac3998eab961ab1d25f
muneeb202 <muneebarshad2002@gmail.com>
2023-10-15 22:38:14 +0500

    Loading Screen Complete

--- envision/src/Generate.js
+++ envision/src/Generate.js
@@ -7,7 +7,8 @@ import Particles from 'react-tsparticles';
 import { loadFull } from 'tsparticles';
 import particlesConfig2 from "./config/particle-config2";
 import animationData from './images/robot.json';
-import Lottie, {LottieRefCurrentProps} from 'lottie-react'
+import Lottie, { LottieRefCurrentProps } from 'lottie-react'
+import Typewriter from 'typewriter-effect';
 
 const theme = createTheme({
     palette: {
@@ -35,11 +36,29 @@ const Generate = () => {
     const [isLoading, setIsLoading] = useState(false);
     const [progress, setProgress] = React.useState(0);
     const [buffer, setBuffer] = React.useState(10);
-    const animationRef = useRef(<LottieRefCurrentProps/>);
+    const animationRef = useRef(<LottieRefCurrentProps />);
+    const [isPlaying, setIsPlaying] = useState(false);
+    const facts = [
+        'The earliest known panorama was created in 1787 by Robert Barker, an Irish painter. It was a massive painting of Edinburgh, Scotland, that was displayed in a specially built rotunda.',
+        'The word "panorama" comes from the Greek words "pan" (all) and "horama" (view).',
+        '360-degree images are often used in virtual reality (VR) experiences to create a sense of immersion.',
+        'Panoramas can be created using a variety of methods, including stitching together multiple photos, using a special 360-degree camera, or even using a smartphone app.',
+        '360-degree images are not just for still photos. You can also create 360-degree videos that capture a full spherical view of a scene.',
+        'The largest 360-degree photo ever taken was of Mont Blanc, the highest mountain in the Alps. The photo was created by stitching together over 70,000 individual images!',
+        '360-degree images are being used in a variety of innovative ways, such as creating virtual tours of real estate properties, museums, and other locations.',
+        'Google Street View is one of the most popular uses of 360-degree images. It allows you to explore cities and towns around the world from the comfort of your own home.',
+        'Facebook also supports 360-degree images, so you can share your immersive photos and videos with your friends and family.',
+        'The first 360-degree camera was invented in 1904 by Julius Neubronner, a German pharmacist.',
+        'The first 360-degree video was shot in 1995 by the Fraunhofer Institute for Computer Graphics Research.',
+        '360-degree images are sometimes called "equirectangular projections" because they are projected onto a sphere.',
+    ]
 
-  const handleClick = () => {
-    animationRef.current?.goToAndPlay(0, true);
-  };
+    const handleClick = () => {
+        if (!isPlaying) {
+            animationRef.current?.goToAndPlay(0, true);
+            setIsPlaying(true);
+        }
+    };
 
     const progressRef = React.useRef(() => { });
     React.useEffect(() => {
@@ -98,8 +117,8 @@ const Generate = () => {
     return (
         <ThemeProvider theme={theme}>
             <MemoizedParticles options={particlesConfig2} />
- 
-            <div> 
+
+            <div>
                 {isLoading && (
                     <div className={isLoading ? 'loading-screen active' : 'loading-screen'} onClick={handleClick}>
                         <Lottie
@@ -107,10 +126,25 @@ const Generate = () => {
                             animationData={animationData}
                             loop={false}
                             style={{ width: 200, height: 200 }}
+                            onComplete={() => setIsPlaying(false)}
                         />
+                        <br/>
                         <Box sx={{ width: '50%' }}>
                             <LinearProgress variant="buffer" value={progress} valueBuffer={buffer} />
                         </Box>
+                        <br/>
+                        <p>
+                            <Typewriter
+                                options={{
+                                    strings: facts,
+                                    autoStart: true,
+                                    loop: true,
+                                    delay: 30,
+                                    deleteSpeed: 0,
+                                    pauseFor: 5000
+                                  }}
+                            />
+                        </p>
                     </div>
                 )}
             </div>
