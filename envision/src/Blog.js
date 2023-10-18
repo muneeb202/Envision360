390bb972dacc7397cce36c8bc9a6b6a7da3fdc59
muneeb202 <muneebarshad2002@gmail.com>
2023-10-18 23:56:26 +0500

    Update blog page

--- envision/src/Blog.js
+++ envision/src/Blog.js
@@ -4,6 +4,7 @@ import logo from './images/image.png'
 import { useState } from 'react';
 import eiffel from './images/eiffel.png'
 import { red } from '@mui/material/colors';
+import './Blog.css'
 
 const theme = createTheme({
     palette: {
@@ -29,7 +30,7 @@ const theme = createTheme({
 
 const Blog = () => {
 
-    const [likes, setLikes] = useState(59);
+    const [likes, setLikes] = useState(Math.floor(Math.random() * 200));
     const [liked, setLiked] = useState(false);
 
     const likePost = () => {
@@ -46,25 +47,28 @@ const Blog = () => {
                 <img src={background} alt='background' />
             </div>
             <a href='/'><img src={logo} className='logo' alt='background' /></a>
-            <div className='d-flex justify-content-center'>
+            <div className='d-flex flex-column align-items-center'>
                 <Card sx={{width: '50vw', backgroundColor:'#000000b5'}} elevation={2}>
                     <CardHeader 
                         avatar={ <Avatar sx={{ bgcolor: red[500] }}>R</Avatar>}
                         action={
-                            <Badge badgeContent={likes}>
+                            <Badge badgeContent={likes} max={99}>
                                 <i style={{fontSize:'24px', color:'red'}} onClick={likePost} class={liked ? "fas fa-heart": "far fa-heart"}></i>
                             </Badge>
                         }
                         title='Eiffel Tower in the Night Sky'
                         subheader='October 17, 2023'
                     />
-                    <Box sx={{display:'flex'}}>
+                    <Box sx={{display:'flex', position:'relative'}}>
                         <CardMedia component='img' sx={{maxHeight:'200px', maxWidth:'200px'}} src={eiffel} alt='eiffel'/>
                         <CardContent>
                             <p style={{fontWeight:'200'}}>Capturing the enchanting beauty of the Eiffel Tower against the night sky, where the city lights weave a mesmerizing tapestry. A breathtaking moment frozen in time. 🌃✨ #EiffelNights #CityscapeMagic #TravelDreams</p>
                         </CardContent>
+                        <i class="fas fa-expand"></i>
                     </Box>
-                </Card>
+                </Card> <br/>
+
+                
             </div>
         </ThemeProvider>
     )
