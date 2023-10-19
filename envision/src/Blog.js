4cffa765f8f0623c45171cf826d7894dee92bd94
muneeb202 <muneebarshad2002@gmail.com>
2023-10-19 17:41:28 +0500

    Simple Dialog Post

--- envision/src/Blog.js
+++ envision/src/Blog.js
@@ -1,7 +1,7 @@
-import { Avatar, Badge, Box, Card, CardContent, CardHeader, CardMedia, IconButton, ThemeProvider, Typography, createTheme } from '@mui/material';
+import { Avatar, Badge, Box, Button, Card, CardContent, CardHeader, CardMedia, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, IconButton, Slide, ThemeProvider, Typography, createTheme } from '@mui/material';
 import background from './images/blogbg.png'
 import logo from './images/image.png'
-import { useState } from 'react';
+import { forwardRef, useState } from 'react';
 import eiffel from './images/eiffel.png'
 import { red } from '@mui/material/colors';
 import './Blog.css'
@@ -26,12 +26,23 @@ const theme = createTheme({
 // Use List for comments
 // Use badge with heart icon for liking comments 
 // Use table for displaying all posts
-
+const Transition = forwardRef(function Transition(props, ref) {
+    return <Slide direction="up" ref={ref} {...props} />;
+});
 
 const Blog = () => {
 
     const [likes, setLikes] = useState(Math.floor(Math.random() * 200));
     const [liked, setLiked] = useState(false);
+    const [open, setOpen] = useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
 
     const likePost = () => {
         if (liked)
@@ -48,27 +59,50 @@ const Blog = () => {
             </div>
             <a href='/'><img src={logo} className='logo' alt='background' /></a>
             <div className='d-flex flex-column align-items-center'>
-                <Card sx={{width: '50vw', backgroundColor:'#000000b5'}} elevation={2}>
-                    <CardHeader 
-                        avatar={ <Avatar sx={{ bgcolor: red[500] }}>R</Avatar>}
+                <Card sx={{ width: '50vw', backgroundColor: '#000000b5' }} elevation={2}>
+                    <CardHeader
+                        avatar={<Avatar sx={{ bgcolor: red[500] }}>R</Avatar>}
                         action={
                             <Badge badgeContent={likes} max={99}>
-                                <i style={{fontSize:'24px', color:'red'}} onClick={likePost} class={liked ? "fas fa-heart": "far fa-heart"}></i>
+                                <i style={{ fontSize: '24px', color: 'red' }} onClick={likePost} class={liked ? "fas fa-heart" : "far fa-heart"}></i>
                             </Badge>
                         }
                         title='Eiffel Tower in the Night Sky'
                         subheader='October 17, 2023'
                     />
-                    <Box sx={{display:'flex', position:'relative'}}>
-                        <CardMedia component='img' sx={{maxHeight:'200px', maxWidth:'200px'}} src={eiffel} alt='eiffel'/>
+                    <Box sx={{ display: 'flex', position: 'relative' }}>
+                        <CardMedia component='img' sx={{ maxHeight: '200px', maxWidth: '200px' }} src={eiffel} alt='eiffel' />
                         <CardContent>
-                            <p style={{fontWeight:'200'}}>Capturing the enchanting beauty of the Eiffel Tower against the night sky, where the city lights weave a mesmerizing tapestry. A breathtaking moment frozen in time. 🌃✨ #EiffelNights #CityscapeMagic #TravelDreams</p>
+                            <p style={{ fontWeight: '200' }}>Capturing the enchanting beauty of the Eiffel Tower against the night sky, where the city lights weave a mesmerizing tapestry. A breathtaking moment frozen in time. 🌃✨ #EiffelNights #CityscapeMagic #TravelDreams</p>
                         </CardContent>
-                        <i class="fas fa-expand"></i>
+                        <i onClick={handleClickOpen} class="fas fa-expand"></i>
                     </Box>
-                </Card> <br/>
+                </Card> <br />
+                <Dialog
+                    open={open}
+                    TransitionComponent={Transition}
+                    keepMounted
+                    onClose={handleClose}
+                    maxWidth='md'
+                >
+                    <Card elevation={2}>
+                        <CardHeader
+                            avatar={<Avatar sx={{ bgcolor: red[500] }}>R</Avatar>}
+                            action={
+                                <Badge badgeContent={likes} max={99}>
+                                    <i style={{ fontSize: '24px', color: 'red' }} onClick={likePost} class={liked ? "fas fa-heart" : "far fa-heart"}></i>
+                                </Badge>
+                            }
+                            title='Eiffel Tower in the Night Sky'
+                            subheader='October 17, 2023'
+                        />
+                        <CardMedia component='img' sx={{ maxHeight: '300px', maxWidth: '100%' }} src={eiffel} alt='eiffel' />
+                        <CardContent>
+                            <p style={{ fontWeight: '200' }}>Capturing the enchanting beauty of the Eiffel Tower against the night sky, where the city lights weave a mesmerizing tapestry. A breathtaking moment frozen in time. 🌃✨ #EiffelNights #CityscapeMagic #TravelDreams</p>
+                        </CardContent>
+                    </Card>
+                </Dialog>
 
-                
             </div>
         </ThemeProvider>
     )
