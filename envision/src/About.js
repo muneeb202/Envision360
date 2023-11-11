165f6fc535e817f1002dc765b5a652383d986968
muneeb202 <muneebarshad2002@gmail.com>
2023-11-11 20:51:29 +0500

    Alignment Fixed

--- envision/src/About.js
+++ envision/src/About.js
@@ -25,19 +25,27 @@ const AboutUs = () => {
                 </div>
             </div>
 
-            <div className='intro'>
-            <img src={introimage1} className='introimage1' alt='introimage1'/>
-            <img src={introimage2} className='introimage2' alt='introimage2'/>
+            <div className='about-section'>
+                <img src={introimage1} className='introimage1' alt='introimage1'/>
+                <img src={introimage2} className='introimage2' alt='introimage2'/>
             </div>
 
             <div className='about-section'>
-                <div className='introimage3-container'>
                     <img src={introimage3} className='introimage3' alt='introimage3'/>
-                </div>
+                {/* <div className='introimage3-container'>
+                </div> */}
                 <div className='about-content'>
                     <h2>About Us</h2>
                     <p>
-                        At Envision360, we are on a passionate journey to redefine the way we perceive and explore the visual world. Our team comprises visionaries, developers, and explorers who share a common belief: that immersive digital experiences should be accessible to all. Our story began with a desire to empower travel enthusiasts, educators, researchers, and urban planners with a versatile tool that seamlessly combines the marvel of wanderlust with the power of cutting-edge technology. We're here to transform passive viewing into an interactive and meaningful journey of discovery, transcending the boundaries of physical travel.
+                        At Envision360, we are on a passionate journey to redefine the way we 
+                        perceive and explore the visual world. Our team comprises visionaries, 
+                        developers, and explorers who share a common belief: that immersive digital 
+                        experiences should be accessible to all. Our story began with a desire to 
+                        empower travel enthusiasts, educators, researchers, and urban planners with 
+                        a versatile tool that seamlessly combines the marvel of wanderlust with the 
+                        power of cutting-edge technology. We're here to transform passive viewing 
+                        into an interactive and meaningful journey of discovery, transcending the 
+                        boundaries of physical travel.
                     </p>
                 </div>
             </div>
