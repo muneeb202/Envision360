cd354c7ccabd718b460996885e4c3a801a34d5eb
muneeb202 <muneebarshad2002@gmail.com>
2023-11-30 01:31:29 +0500

    Home Sidebar

--- envision/src/Home.js
+++ envision/src/Home.js
@@ -5,21 +5,25 @@ import particlesConfig from "./config/particle-config";
 import Footer from './components/Footer';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import { Avatar, Drawer, IconButton, Tooltip } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
 
 const Home = () => {
     const user = localStorage.getItem('user');
+    const navigate = useNavigate();
     console.log(user)
-    const [name , setName] = useState('');
+    const [name, setName] = useState('');
+    const [sidebar, setSidebar] = useState(false);
 
     useEffect(() => {
         const getUser = async () => {
             try {
-                const response = await axios.post('http://localhost:8000/api/get_user/', {token: user})
+                const response = await axios.post('http://localhost:8000/api/get_user/', { token: user })
                 console.log(response)
                 setName(response.data['name']);
             }
-            catch(e) {
+            catch (e) {
                 console.log(e);
             }
         }
@@ -40,40 +44,65 @@ const Home = () => {
             <div className='image-container'>
                 <img src={`${process.env.PUBLIC_URL}/images/homebg.png`} alt='background' draggable='false' />
             </div>
-            <nav class="navbar navbar-expand-lg navbar-dark ">
+            <div className='navbar'>
                 <img src={`${process.env.PUBLIC_URL}/images/Logo Small.png`} className='logo' alt='logo' draggable='false' />
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-                    <div className='home-navbar'>
-                        <div className='links'>
-                            <ul class="navbar-nav">
-                                <li class="nav-item">
-                                    {name ? (
-                                        <>
-                                            <p>{name}</p>
-                                            <a class="nav-link" onClick={userLogout}>Logout</a>
-                                        </>
-                                    ) :
-                                        (
-                                            <a href='/start'>Get Started </a>
-                                        )}
-                                </li>
-                                <li class="nav-item">
-                                    <a href='/about'>About Us</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href='/blog'>Blog</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="demoButton" href='/generate'>{user ? 'Generate' : 'Demo'}</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
+                <div className=' d-none d-md-block'>
+                <div className='links'>
+                    {name ? (
+                        <>
+                            <a onClick={userLogout}>Logout</a>
+                        </>
+                    ) :
+                        (
+                            <a href='/start'>Get Started</a>
+                        )}
+                    <a href='/aboutus'>About Us</a>
+                    <a href='/blog'>Blog</a>
+                    <a className='demoButton' href='/generate'>{name ? 'Generate' : 'Demo'}</a>
+                    {name && (
+                        <Tooltip title='Click to view profile'> 
+                            <IconButton onClick={() => navigate('/profile')}><Avatar sx={{ bgcolor: '#09687d' }}>{name.charAt(0).toUpperCase()}</Avatar></IconButton>
+                        </Tooltip>
+                    )}
                 </div>
-            </nav>
+                </div>
+                <div className='home-navbar d-md-none'>
+                <Tooltip title='Click to view more details'> 
+                    <IconButton className='m-5' onClick={() => setSidebar(true)}><i className="fa-solid fa-ellipsis-vertical" style={{ color: '#ffffff', fontSize: 'x-large' }}></i></IconButton>
+                </Tooltip>
+                    <Drawer className='home-sidebar' open={sidebar} anchor='right' onClose={() => setSidebar(false)}>
+                        <div className='sidebar'>
+                            <div className='links'>
+                                <ul class="navbar-nav" style={{ textAlign: 'center' }}>
+                                    <li class="nav-item">
+                                        {name ? (
+                                            <div style={{ marginBottom: '10px', display: 'flex', justifyContent: 'center' }}>
+                                                <a href='/profile'><Avatar style={{ height: '80px', width: '80px', fontSize: 'x-large' }} sx={{ bgcolor: '#09687d' }}>{name.charAt(0).toUpperCase()}</Avatar></a>
+                                            </div>
+                                        ) :
+                                            (
+                                                <a class="nav-link" href='/start'>Get Started </a>
+                                            )}
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href='/aboutus'>About Us</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href='/blog'>Blog</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href='/generate'>{user ? 'Generate' : 'Demo'}</a>
+                                    </li>
+                                </ul>
+
+                                <div class="nav-item logout-button" style={{ marginLeft: '30px', position: 'absolute', bottom: '0', textAlign: 'center' }} >
+                                    {user && <a class="nav-link" href onClick={userLogout}>Logout</a>}
+                                </div>
+                            </div>
+                        </div>
+                    </Drawer>
+                </div>
+            </div>
 
             <div className='header'>
                 <h1>ENVISION360</h1>
@@ -88,8 +117,9 @@ const Home = () => {
                     style={{ position: 'absolute', right: '5vw', bottom: '20vh' }}
                 />
             </div>
-            <Footer top='85vh'/>
+            <Footer top='85vh' />
         </div>
+
     );
 }
 
