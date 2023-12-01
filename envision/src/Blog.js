91a2f085fb7dfc5f7ac85a160b52b8bcf67c37ff
Wajeeha <wajeehazulfiqar48@gmail.com>
2023-12-02 00:37:12 +0500

    Blog post and comments generalized

--- envision/src/Blog.js
+++ envision/src/Blog.js
@@ -1,7 +1,8 @@
-import { Avatar, Badge, Box, Button, Card, CardContent, CardHeader, CardMedia, Collapse, Dialog, IconButton, List, ListItem, ListItemAvatar, ListItemText, Slide, ThemeProvider, Tooltip, createTheme } from '@mui/material';
-import { forwardRef, useState } from 'react';
+import { Avatar, Badge, Box, Button, Card, CardContent, CardHeader, CardMedia, Collapse, Dialog, IconButton, List, ListItem, ListItemAvatar, ListItemText, Slide, TextField, ThemeProvider, Tooltip, createTheme } from '@mui/material';
+import { forwardRef, useEffect, useState } from 'react';
 import { blue, purple, red } from '@mui/material/colors';
 import './Blog.css'
+import axios from 'axios';
 
 const theme = createTheme({
     palette: {
@@ -32,11 +33,14 @@ const Blog = () => {
     const [likes, setLikes] = useState(Math.floor(Math.random() * 200));
     const [liked, setLiked] = useState(false);
     const [open, setOpen] = useState(false);
-    const [comments, setComments] = useState(false);
+    const [comments, setComments] = useState([]);
+    const [Showcomments, setShowComments] = useState(false);
+    const [blogPosts, setBlogPosts] = useState([]);
+    const user = localStorage.getItem('user');
 
-    const handleClickOpen = () => {
-        setOpen(true);
-    };
+    // const handleClickOpen = () => {
+    //     setOpen(true);
+    // };
 
     const handleClose = () => {
         setOpen(false);
@@ -50,6 +54,68 @@ const Blog = () => {
         setLiked(!liked);
     }
 
+    useEffect(() => {
+        const fetchBlogPosts = async () => {
+            try {
+                const response = await axios.get('http://127.0.0.1:8000/api/blogposts/');
+                setBlogPosts(response.data);
+            } catch (error) {
+                console.error('Error fetching blog posts:', error);
+            }
+        };
+
+        fetchBlogPosts();
+    }, []);
+
+    const [selectedPost, setSelectedPost] = useState(null);
+
+
+    const handleOpenDialog = (post) => {
+        setSelectedPost(post);
+        setOpen(true);
+    };
+
+    const [comment, setComment] = useState('');
+
+    const handleCommentChange = (event) => {
+        setComment(event.target.value);
+    };
+
+    const postComment = async () => {
+        try {
+            const response = await axios.post(
+                'http://127.0.0.1:8000/api/comment/',
+                {
+                    token: localStorage.getItem('user'),
+                    image_id: selectedPost.id,
+                    comment_description: comment,
+                }
+            );
+            console.log('Comment posted successfully:', response.data);
+        } catch (error) {
+            console.error('Error posting comment:', error.response.data);
+        }
+    };
+
+    useEffect(() => {
+        if (selectedPost) {
+            fetchComments(selectedPost.id);
+        }
+    }, [selectedPost]);
+
+    const fetchComments = async (post_id) => {
+        try {
+            const response = await axios.get('http://localhost:8000/api/comment/', {
+                params: {
+                    token: localStorage.getItem('user'),
+                    post_id: post_id
+                }
+            });
+            setComments(response.data);
+        } catch (error) {
+            console.error('Error fetching comments:', error);
+        }
+    };
     return (
         <div style={{ height: 'max-content', minHeight: '100vh' }}>
             <ThemeProvider theme={theme}>
@@ -58,25 +124,29 @@ const Blog = () => {
                 </div>
                 <a href='/'><img style={{ paddingTop: '10px' }} src={`${process.env.PUBLIC_URL}/images/newLogo.png`} className='logo' alt='background' draggable='false' /></a>
                 <div className='d-flex flex-column align-items-center'>
-                    <Card sx={{ width: '50vw', backgroundColor: '#000000b5' }} elevation={2}>
-                        <CardHeader
-                            avatar={<Tooltip title='Ramon Sanchez'><Avatar sx={{ bgcolor: red[500] }}>R</Avatar></Tooltip>}
-                            action={
-                                <Badge badgeContent={likes} max={99}>
-                                    <i style={{ fontSize: '24px', color: 'red' }} onClick={likePost} class={liked ? "fas fa-heart" : "far fa-heart"}></i>
-                                </Badge>
-                            }
-                            title='Eiffel Tower in the Night Sky'
-                            subheader='October 17, 2023'
-                        />
-                        <Box sx={{ display: 'flex', position: 'relative' }}>
-                            <CardMedia component='img' sx={{ maxHeight: '200px', maxWidth: '200px' }} src={`${process.env.PUBLIC_URL}/images/eiffel.png`} draggable='false' alt='eiffel' />
-                            <CardContent>
-                                <p style={{ fontWeight: '200' }}>Capturing the enchanting beauty of the Eiffel Tower against the night sky, where the city lights weave a mesmerizing tapestry. A breathtaking moment frozen in time. 🌃✨ #EiffelNights #CityscapeMagic #TravelDreams</p>
-                            </CardContent>
-                            <IconButton sx={{ position: 'absolute', bottom: 10, right: 10, fontSize: 20 }}><i onClick={handleClickOpen} className="fas fa-expand"></i></IconButton>
-                        </Box>
-                    </Card> <br />
+                    {blogPosts.map((post) => (
+                        <Card key={post.id} sx={{ width: '50vw', backgroundColor: '#000000b5' }} elevation={2}>
+                            <CardHeader
+                                avatar={<Tooltip title={post.user}><Avatar sx={{ bgcolor: red[500] }}>{post.user}</Avatar></Tooltip>}
+                                action={
+                                    <Badge badgeContent={likes} max={99}>
+                                        <i style={{ fontSize: '24px', color: 'red' }} onClick={likePost} class={liked ? "fas fa-heart" : "far fa-heart"}></i>
+                                    </Badge>
+                                }
+                                title={post.title}
+                                subheader={post.created_date}
+                            />
+                            <Box sx={{ display: 'flex', position: 'relative' }}>
+                                <CardMedia component='img' sx={{ maxHeight: '200px', maxWidth: '200px' }} src={'http://127.0.0.1:8000' + post.image} draggable='false' alt={post.title} />
+                                <CardContent>
+                                    <p style={{ fontWeight: '200' }}>{post.description}</p>
+                                </CardContent>
+                                <IconButton sx={{ position: 'absolute', bottom: 10, right: 10, fontSize: 20 }}>
+                                    <i onClick={() => handleOpenDialog(post)} className="fas fa-expand"></i>
+                                </IconButton>
+                            </Box>
+                        </Card>
+                    ))}
                     <Dialog
                         open={open}
                         TransitionComponent={Transition}
@@ -84,57 +154,43 @@ const Blog = () => {
                         onClose={handleClose}
                         maxWidth='md'
                     >
-                        <Card elevation={2}>
-                            <CardHeader
-                                avatar={<Tooltip title='Ramon Sanchez'><Avatar sx={{ bgcolor: red[500] }}>R</Avatar></Tooltip>}
-                                action={
-                                    <Badge badgeContent={likes} max={99}>
-                                        <i style={{ fontSize: '24px', color: 'red' }} onClick={likePost} class={liked ? "fas fa-heart" : "far fa-heart"}></i>
-                                    </Badge>
-                                }
-                                title='Eiffel Tower in the Night Sky'
-                                subheader='October 17, 2023'
-                            />
-                            <CardMedia component='img' sx={{ maxHeight: '300px', maxWidth: '100%' }} draggable='false' src={`${process.env.PUBLIC_URL}/images/eiffel.png`} alt='eiffel' />
-                            <CardContent>
-                                <p style={{ fontWeight: '200' }}>Capturing the enchanting beauty of the Eiffel Tower against the night sky, where the city lights weave a mesmerizing tapestry. A breathtaking moment frozen in time. 🌃✨ #EiffelNights #CityscapeMagic #TravelDreams</p>
-                            </CardContent>
-                            <Button variant='contained' onClick={() => setComments(!comments)} sx={{ width: '100%', backgroundColor: '#6666667d', letterSpacing: '2px' }}>{comments ? 'Hide' : 'View'} Comments</Button>
-                            <Collapse in={comments} timeout="auto" unmountOnExit>
-                                <List dense={true} sx={{ bgcolor: '#66666630' }}>
-                                    <ListItem>
-                                        <ListItemAvatar><Avatar sx={{ bgcolor: blue[700], height: '2vw', width: '2vw', fontSize: '15px' }}>M</Avatar></ListItemAvatar>
-                                        <ListItemText primary='Comment 1' secondary='October 19, 2023' />
-                                    </ListItem>
-                                    <ListItem>
-                                        <ListItemAvatar><Avatar sx={{ bgcolor: purple[700], height: '2vw', width: '2vw', fontSize: '15px' }}>W</Avatar></ListItemAvatar>
-                                        <ListItemText primary='Comment 2' secondary='October 21, 2023' />
-                                    </ListItem>
-                                    <ListItem>
-                                        <ListItemAvatar><Avatar sx={{ bgcolor: blue[700], height: '2vw', width: '2vw', fontSize: '15px' }}>M</Avatar></ListItemAvatar>
-                                        <ListItemText primary='Comment 1' secondary='October 19, 2023' />
-                                    </ListItem>
-                                    <ListItem>
-                                        <ListItemAvatar><Avatar sx={{ bgcolor: purple[700], height: '2vw', width: '2vw', fontSize: '15px' }}>W</Avatar></ListItemAvatar>
-                                        <ListItemText primary='Comment 2' secondary='October 21, 2023' />
-                                    </ListItem><ListItem>
-                                        <ListItemAvatar><Avatar sx={{ bgcolor: blue[700], height: '2vw', width: '2vw', fontSize: '15px' }}>M</Avatar></ListItemAvatar>
-                                        <ListItemText primary='Comment 1' secondary='October 19, 2023' />
-                                    </ListItem>
-                                    <ListItem>
-                                        <ListItemAvatar><Avatar sx={{ bgcolor: purple[700], height: '2vw', width: '2vw', fontSize: '15px' }}>W</Avatar></ListItemAvatar>
-                                        <ListItemText primary='Comment 2' secondary='October 21, 2023' />
-                                    </ListItem><ListItem>
-                                        <ListItemAvatar><Avatar sx={{ bgcolor: blue[700], height: '2vw', width: '2vw', fontSize: '15px' }}>M</Avatar></ListItemAvatar>
-                                        <ListItemText primary='Comment 1' secondary='October 19, 2023' />
-                                    </ListItem>
-                                    <ListItem>
-                                        <ListItemAvatar><Avatar sx={{ bgcolor: purple[700], height: '2vw', width: '2vw', fontSize: '15px' }}>W</Avatar></ListItemAvatar>
-                                        <ListItemText primary='Comment 2' secondary='October 21, 2023' />
-                                    </ListItem>
-                                </List>
-                            </Collapse>
-                        </Card>
+                        {selectedPost && (
+                            <Card elevation={2}>
+                                <CardHeader
+                                    avatar={<Tooltip title={selectedPost.user}><Avatar sx={{ bgcolor: red[500] }}>{selectedPost.user}</Avatar></Tooltip>}
+                                    action={
+                                        <Badge badgeContent={likes} max={99}>
+                                            <IconButton style={{ fontSize: '24px', color: 'red' }} onClick={likePost}>
+                                                <i className={liked ? "fas fa-heart" : "far fa-heart"}></i>
+                                            </IconButton>
+                                        </Badge>
+                                    }
+                                    title={selectedPost.title}
+                                    subheader={selectedPost.created_date}
+                                />
+                                <CardMedia component='img' sx={{ maxHeight: '300px', maxWidth: '100%' }} draggable='false' src={'http://127.0.0.1:8000' + selectedPost.image} alt={selectedPost.title} />
+                                <CardContent>
+                                    <p style={{ fontWeight: '200' }}>{selectedPost.description}</p>
+                                </CardContent>
+                                <TextField style={{ width: '100%' }} id="outlined-basic" label="Add a comment" variant="outlined" value={comment} onChange={handleCommentChange} />
+                                <Button disabled={comment.length === 0} variant='contained' onClick={postComment} sx={{ width: '100%', backgroundColor: '#6666667d', letterSpacing: '2px' }}>Post Comment</Button>
+                                <Button variant='contained' onClick={() => setShowComments(!Showcomments)} sx={{ width: '100%', backgroundColor: '#6666667d', letterSpacing: '2px' }}>{Showcomments ? 'Hide' : 'View'} Comments</Button>
+                                <Collapse in={Showcomments} timeout="auto" unmountOnExit>
+                                    <List dense={true} sx={{ bgcolor: '#66666630' }}>
+                                        {Object.values(comments).map((comment, index) => (
+                                            <ListItem key={comment.id}>
+                                                <ListItemAvatar>
+                                                    <Avatar sx={{ bgcolor: index % 2 === 0 ? blue[700] : purple[700], height: '2vw', width: '2vw', fontSize: '15px' }}>
+                                                        {comment.user}
+                                                    </Avatar>
+                                                </ListItemAvatar>
+                                                <ListItemText primary={comment.description} secondary={comment.created_date.slice(0, 10)} />
+                                            </ListItem>
+                                        ))}
+                                    </List>
+                                </Collapse>
+                            </Card>
+                        )}
                     </Dialog>
 
                 </div>
--- envision_backend/api/serializers.py
+++ envision_backend/api/serializers.py
@@ -1,12 +1,21 @@
 from rest_framework import serializers
-from .models import Image
+from .models import Image, Comment
+
 
 class ImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Image
-        fields = ['id', 'title', 'description', 'image', 'created_date', 'posted', 'favourite', 'likes']
+        fields = "__all__"
 
     def to_representation(self, instance):
         representation = super().to_representation(instance)
-        representation['created_date'] = instance.created_date.strftime('%d %B %Y')  # Format the date as '20 September 2023'
+        representation["created_date"] = instance.created_date.strftime(
+            "%d %B %Y"
+        )  # Format the date as '20 September 2023'
         return representation
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = "__all__"
