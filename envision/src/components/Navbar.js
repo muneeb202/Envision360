731e1521794c2527a5c056010c90fde164e63111
muneeb202 <muneebarshad2002@gmail.com>
2023-12-03 17:37:21 +0500

    Components

---
+++ envision/src/components/Navbar.js
@@ -0,0 +1,117 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Avatar, Drawer, IconButton, Tooltip } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+    const navigate = useNavigate();
+    const [sidebar, setSidebar] = useState(false);
+    const [name, setName] = useState('');
+
+    const userLogout = (e) => {
+        e.preventDefault();
+        localStorage.removeItem('user');
+        window.location.reload();
+    }
+
+    const user = localStorage.getItem('user');
+
+    useEffect(() => {
+        const getUser = async () => {
+            try {
+                const response = await axios.post('http://localhost:8000/api/get_user/', { token: user })
+                console.log(response)
+                setName(response.data['name']);
+            }
+            catch (e) {
+                console.log(e);
+            }
+        }
+        getUser();
+    }, [])
+
+    function stringToColor(string) {
+        let hash = 0;
+        let i;
+
+        /* eslint-disable no-bitwise */
+        for (i = 0; i < string.length; i += 1) {
+            hash = string.charCodeAt(i) + ((hash << 5) - hash);
+        }
+
+        let color = '#';
+
+        for (i = 0; i < 3; i += 1) {
+            const value = (hash >> (i * 8)) & 0xff;
+            color += `00${value.toString(16)}`.slice(-2);
+        }
+        return color
+    }
+
+    return (
+        <div className='navbar'>
+            <img src={`${process.env.PUBLIC_URL}/images/newLogo.png`} className='logo' alt='logo' draggable='false' />
+            <div className=' d-none d-md-block'>
+                <div className='links'>
+                    {name ? (
+                        <>
+                            <a href='#' onClick={userLogout}>Logout</a>
+                        </>
+                    ) :
+                        (
+                            <a href='/start'>Get Started</a>
+                        )}
+                    <a href='/aboutus'>About Us</a>
+                    <a href='/blog'>Blog</a>
+                    <a className='demoButton' href='/generate'>{name ? 'Generate' : 'Demo'}</a>
+                    {name && (
+                        <Tooltip title='Click to view profile'>
+                            <Link to='/profile'><Avatar sx={{ bgcolor: stringToColor(name), color:'black' }}>{name.charAt(0).toUpperCase()}</Avatar></Link>
+                        </Tooltip>
+                    )}
+                </div>
+            </div>
+            <div className='home-navbar d-md-none'>
+                <Tooltip title='Click to view more details'>
+                    <IconButton className='m-5' onClick={() => setSidebar(true)}><i className="fa-solid fa-ellipsis-vertical" style={{ color: '#ffffff', fontSize: 'x-large' }}></i></IconButton>
+                </Tooltip>
+                <Drawer className='home-sidebar' open={sidebar} anchor='right' onClose={() => setSidebar(false)}>
+                    <div className='sidebar'>
+                        <div className='links'>
+                            <ul className="navbar-nav" style={{ textAlign: 'center' }}>
+                                <li className="nav-item">
+                                    {name ? (
+                                        <div style={{ marginBottom: '10px', display: 'flex', justifyContent: 'center' }}>
+                                            <Tooltip title='Click to view profile'>
+                                                <Link to='/profile'><Avatar style={{ height: '80px', width: '80px', fontSize: 'x-large' }} sx={{ bgcolor: stringToColor(name), color:'black' }}>{name.charAt(0).toUpperCase()}</Avatar></Link>
+                                            </Tooltip>
+                                        </div>
+                                    ) :
+                                        (
+                                            <Link to={'/start'} >Get Started</Link>
+                                        )}
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href='/aboutus'>About Us</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href='/blog'>Blog</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href='/generate'>{user ? 'Generate' : 'Demo'}</a>
+                                </li>
+                            </ul>
+
+                            <div className="nav-item logout-button" style={{ position: 'absolute', bottom: '0', textAlign: 'center' }} >
+                                {user && <a className="nav-link" href onClick={userLogout}>Logout</a>}
+                            </div>
+                        </div>
+                    </div>
+                </Drawer>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
---
+++ envision/src/components/Sidebar.js
@@ -0,0 +1,74 @@
+import { Avatar, Drawer, IconButton, Tooltip } from "@mui/material";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+
+const Sidebar = () => {
+    const [sidebar, setSidebar] = useState(false);
+    const [user, setUser] = useState({ name: ' ', email: ' ' });
+    const location = useLocation();
+
+    const isActive = (path) => {
+        return location.pathname === path ? 'active' : '';
+    };
+
+    useEffect(() => {
+        const getUser = async () => {
+            try {
+                const response = await axios.post('http://localhost:8000/api/get_user/', { token: localStorage.getItem('user') })
+                console.log(response)
+                setUser(response.data);
+            }
+            catch (e) {
+                console.log(e);
+                setUser(null)
+            }
+        }
+        getUser();
+    }, [])
+
+    function stringToColor(string) {
+        let hash = 0;
+        let i;
+
+        /* eslint-disable no-bitwise */
+        for (i = 0; i < string.length; i += 1) {
+            hash = string.charCodeAt(i) + ((hash << 5) - hash);
+        }
+
+        let color = '#';
+
+        for (i = 0; i < 3; i += 1) {
+            const value = (hash >> (i * 8)) & 0xff;
+            color += `00${value.toString(16)}`.slice(-2);
+        }
+        return color
+    }
+
+    return (
+        <div className='home-navbar d-flex justify-content-between'>
+            <a href='/'><img style={{ paddingTop: '10px' }} src={`${process.env.PUBLIC_URL}/images/newLogo.png`} className='logo' alt='background' draggable='false' /></a>
+            {user &&
+                <>
+                    <IconButton className='m-5' onClick={() => setSidebar(true)}><Tooltip title='Click to view more details'><Avatar sx={{ bgcolor: stringToColor(user.name) }}>{user.name.charAt(0).toUpperCase()}</Avatar></Tooltip></IconButton>
+                    <Drawer className={`${location.pathname.slice(1)}-container`} open={sidebar} anchor='right' onClose={() => setSidebar(false)}>
+                        <div className='sidebar '>
+                            <Avatar sx={{ bgcolor: stringToColor(user.name), height: 100, width: 100, fontSize: 30 }}>{user.name.charAt(0).toUpperCase()}</Avatar><br />
+                            <h4>{user.name.split(' ')
+                                .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+                                .join(' ')}</h4><br />
+                            <p>{user.email}</p>
+                            <p><span>Joined: </span>20 October 2023</p>
+                            <br />
+                            <Link to='/generate' className={isActive('/generate')}>Generate</Link>
+                            <Link to='/blog' className={isActive('/blog')}>Blog</Link>
+                            <Link to='/profile' className={isActive('/profile')}>Profile</Link>
+                        </div>
+                    </Drawer>
+                </>
+            }
+        </div>
+    )
+}
+
+export default Sidebar;
